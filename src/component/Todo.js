7429a052840fde3df4ca53597ec243a7f315483b
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-19 01:24:10 +0600

    css style added

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -8,10 +8,13 @@ const Todo = () => {
             <h1 className='title'>Add Your Task Here</h1>
             </div>
             <div className='add-task'>
-                <input type="text" name="name" placeholder='Task Name' id="" />
-                <input type="text" name="name" placeholder='Description' id="" />
+                <label htmlFor="name" className='input-title'>Task Name:</label> <br />
+                <input type="text" name="name" placeholder='Task Name' id="" /> <br />
+                <label htmlFor="description" className='input-title'>Description:</label> <br />
+                <textarea type="text" name="description" placeholder='Description' id="" className='textarea-description' /> <br />
+                <button className='input-button'>Add Task</button>
             </div>
-            <button>Add Task</button>
+            
         </div>
     );
 };
